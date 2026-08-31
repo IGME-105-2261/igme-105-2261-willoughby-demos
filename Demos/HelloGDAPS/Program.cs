a1e5f2d101044080fc98a3292bbe1bcd0a2b4c4b
Austin Willoughby <arwigm@rit.edu>
2026-08-31 10:04:10 -0400

    Setup repo

---
+++ Demos/HelloGDAPS/Program.cs
@@ -0,0 +1,46 @@
+﻿//Austin Willoughby - arwigm@rit.edu
+//Comment at the top of the file explaining the code
+
+namespace HelloGDAPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Explaining the next line
+            Console.WriteLine("Hello, World!");
+            /*
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            */
+            Console.WriteLine("Hello, Class!");
+
+            Console.Clear();
+
+            Console.Write("Hello World");
+            Console.WriteLine("Testing");
+
+
+            Console.WriteLine("Hello " + "World!" + 7);
+            Console.WriteLine(6 + 3);
+
+            Console.WriteLine(8 % 2);
+            Console.Clear();
+
+            Console.WriteLine(1.0 / 2.0);
+            Console.WriteLine(5.0 / 2);
+            Console.Clear();
+
+            //Console.WriteLine("Six" + (10 - 12));
+            Console.WriteLine(
+                3.14159 *
+                8.2 *
+                8.2);
+
+            Console.WriteLine(3.14159 * 8.2 * 8.2);
+            Console.WriteLine("11111111111111111111111111111111111111111" +
+                "1111111111111111111111111111111111111111111111111111111" +
+                "1111111111111111111111111111111111111111111111111111111111");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Prof. Mesh's in-class demo code for IGME-105 - Fall 2026
+You may *reference* this code and copy portions to your own Demos/ folder, but you may NOT use it verbatim in graded assignments.
