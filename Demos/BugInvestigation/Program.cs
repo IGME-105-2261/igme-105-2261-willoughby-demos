c8a3ffbfe76aeff92a695ca97528917013e93584
Austin Willoughby <arwigm@rit.edu>
2026-09-04 13:02:25 -0400

    Friday week 2

---
+++ Demos/BugInvestigation/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace BugInvestigation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVariable = 5;
+            int divisor = 3;
+            //...
+            divisor -= 2;
+            //...
+            Console.WriteLine(myVariable / divisor);
+
+
+            double myDouble = 100.0;
+            Console.WriteLine(myDouble / 0);
+            Console.WriteLine(0.0 / 0.0);
+        }
+    }
+}
---
+++ Demos/DataAndTypes/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace DataAndTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        // | declaration|| initialization |
+            int myInteger = 5;
+
+            int secondInteger;
+            secondInteger = 10;
+
+            // Assignment
+            myInteger = secondInteger;
+            secondInteger = 12;
+
+            Console.WriteLine("myInteger = " + myInteger);
+            Console.WriteLine("secondInteger = " + secondInteger);
+
+            string myString = "Hello World";
+            Console.WriteLine(myString);
+
+
+            int x = 2;
+            double price = 1.99;
+            double money = 100;
+            int pizzaSlices = 35;
+            int candy = 75;
+            int students = 22;
+
+            int y = 5 + 3;
+            x = y + 3;
+
+            price = price * 1.08;
+            //price *= 1.08; //This is the same as the line above
+
+            money = money - price;
+            //money -= price;
+
+            money = money - (price * pizzaSlices);
+            //money -= price * pizzaSlices;
+
+            candy = candy % students;
+            //candy %= students;
+
+            x = x + 1;
+            x += 1;
+            x++;
+
+            x = x - 1;
+            x -= 1;
+            x--;
+
+            Console.WriteLine("x: " + x);
+            Console.WriteLine("price: " + price);
+            Console.WriteLine("money: " + money);
+            Console.WriteLine("candy: " + candy);
+
+            int myInt = 4;
+            int myInt = 5;
+
+        }
+    }
+}
